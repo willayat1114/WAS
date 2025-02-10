cfedf0d9c6183bc164d1b094a048711c0a58c959
Wilayat <151915178+willayat1114@users.noreply.github.com>
2025-02-10 10:59:10 +0530

    Update WAS.bat

--- WAS.bat
+++ WAS.bat
@@ -14,9 +14,9 @@ echo       Activation Menu
 echo ==============================
 echo 1. Get Windows Activated
 echo 2. Get Office Activated
+echo 3. Install Office
 echo 0. Main Menu
 echo ==============================
-echo Use 0,1 and 2 to select your choice
 set /p choice=Enter your choice: 
 
 if "%choice%"=="1" (
@@ -27,6 +27,31 @@ if "%choice%"=="1" (
 if "%choice%"=="2" (
     powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://get.activated.win | iex; 2"
     goto end
+)
+
+if "%choice%"=="3" (
+    echo Installing Office...
+    cd /d "%~dp0Office"
+    if exist ODT.exe (
+        ODT.exe
+        echo ODT.exe executed successfully.
+    ) else (
+        echo ERROR: ODT.exe not found!
+        pause
+        goto menu
+    )
+
+    if exist setup.exe (
+        echo Running Office Setup...
+        setup.exe /configure configuration.xml
+    ) else (
+        echo ERROR: setup.exe not found!
+        pause
+        goto menu
+    )
+    echo Office installation complete!
+    pause
+    goto menu
 )
 
 if "%choice%"=="0" (
